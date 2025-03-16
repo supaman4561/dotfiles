0d33baf38a17ef1a980a88d771c7164928fb32fd
supaman4561 <dosu416@gmail.com>
2025-03-16 16:48:55 +0900

    change flavour

--- .config/nvim/lua/extensions/colorschemes/catppuccin.lua
+++ .config/nvim/lua/extensions/colorschemes/catppuccin.lua
@@ -1,7 +1,7 @@
 local catppuccin = require('catppuccin')
 
 catppuccin.setup({
-	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	flavour = "frappe", -- latte, frappe, macchiato, mocha
 	transparent_background = true,
 	integrations = {
 		cmp = true,
