4844bf9ce8b4a75ce65cc449ed7a6f3c5e7a7147
supaman4561 <dosu416@gmail.com>
2026-02-07 14:48:32 +0900

    update tmux.conf

--- .config/nvim/lua/extensions/cmp.lua
+++ .config/nvim/lua/extensions/cmp.lua
@@ -1,4 +1,6 @@
 local cmp = require("cmp")
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -21,11 +23,18 @@ cmp.setup({
 		['<C-l>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		["<C-f>"] = cmp.mapping.confirm { select = true },
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 	}),
 	experimental = {
 		ghost_text = false,
 	},
 })
+
+-- autopairs との統合
+cmp.event:on(
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
+)
 -- cmp.setup.cmdline('/', {
 --   mapping = cmp.mapping.preset.cmdline(),
 --   sources = {
